a3359ba85dcfa7c6bbcbe68555000183e9aab25e
Manuel Novoa <novoa@Manuels-MacBook-Air.local>
2020-08-10 10:05:57 -0500

    0.8.2

---
+++ client/src/actions/filter.js
@@ -0,0 +1,12 @@
+import {CONTRACT_LENGTH_FILTER_FAIL, CONTRACT_LENGTH_FILTER_SUCCESS, PROVIDER_FILTER_FAIL, PROVIDER_FILTER_SUCCESS} from './types';
+import { compose } from 'redux';
+
+const config ={
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
+export const filterByProvider = (rates) =>{
+    console.log();
+}
